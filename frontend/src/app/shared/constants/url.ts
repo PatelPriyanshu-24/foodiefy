0e38c01b69e87c07a4f2302e69e10496c01eefe2
PatelPriyanshu-24 <159017504+PatelPriyanshu-24@users.noreply.github.com>
2024-03-13 18:32:42 +0530

    patch update

--- frontend/src/app/shared/constants/url.ts
+++ frontend/src/app/shared/constants/url.ts
@@ -1,4 +1,4 @@
-const BASE_URL=' http://localhost:5000';
+const BASE_URL=' https://gofood-server.vercel.app';
 export const FOOD_URL = BASE_URL +'/api/food';
 export const FOOD_TAG_URL = BASE_URL +'/api/food/tag';
 export const FOOD_BY_SEARCH_URL = BASE_URL +'/api/food/search/';
